ab2f82d0fe46970437509f624dcb97ae4bdd606a
nazimai <809670160@qq.com>
2021-07-30 19:12:48 +0800

    Initial Commit

---
+++ SymbolCrashLog/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SymbolCrashLog
+//
+//  Created by nazimai on 2021/7/30.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SymbolCrashLog/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SymbolCrashLog
+//
+//  Created by nazimai on 2021/7/30.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
